91ef8a103d0aca99175259280b3ad52fe5abe674
ancypaul95 <ancypaul9559@gmail.com>
2018-01-05 12:21:49 +0530

    Finish test

--- test/integration/image_uploading_test.rb
+++ test/integration/image_uploading_test.rb
@@ -11,5 +11,9 @@ class ImageUploadingTest < ActionDispatch::IntegrationTest
                                           password: 'password' } }
     get edit_employee_path(@employee)
     assert_template 'employees/edit'
+    assert_select 'input[type="file"]'
+    image = fixture_file_upload('test/fixtures/rails.png', 'image/png')
+    patch employee_path(@employee), params: {employee: { image: image }}
+    assert @employee.image.blank?
   end
 end
